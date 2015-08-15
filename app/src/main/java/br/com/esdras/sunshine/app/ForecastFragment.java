0feb450fd7785d9f2cebc643797415f77d8d00ca
Esdras <servoesdras@gmail.com>
2015-08-15 00:29:11 -0300

    Lesson 2 Complete

--- app/src/main/java/br/com/esdras/sunshine/app/ForecastFragment.java
+++ app/src/main/java/br/com/esdras/sunshine/app/ForecastFragment.java
@@ -1,14 +1,23 @@
 package br.com.esdras.sunshine.app;
 
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.text.format.Time;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -16,6 +25,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,6 +34,9 @@ import java.util.List;
  * A placeholder fragment containing a simple view.
  */
 public class ForecastFragment extends Fragment {
+
+    private ArrayAdapter<String> adapter;
+
 
     private static final List<String> fakeData = new ArrayList<String>() {{
         add("Today - Sunny - 88/63");
@@ -37,10 +50,34 @@ public class ForecastFragment extends Fragment {
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(item.getItemId() == R.id.action_refresh){
+            new FecthWeatherTask().execute("94043");
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.forecastfragment, menu);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        ArrayAdapter<String> adapter =
+        adapter =
                 new ArrayAdapter<String>(getActivity(),
                                          R.layout.list_item_forecast,
                                          R.id.list_item_forecast_textview,
@@ -55,10 +92,78 @@ public class ForecastFragment extends Fragment {
         return view;
     }
 
-    public class FecthWeatherTask extends AsyncTask<String,Void,String>{
+    public class FecthWeatherTask extends AsyncTask<String,Void,String[]>{
+
+        private final String LOG_TAG = FecthWeatherTask.class.getSimpleName();
+
+        private String getReadableDateString(long time){
+            SimpleDateFormat shortenedDateFormat = new SimpleDateFormat("EEE MMM dd");
+            return shortenedDateFormat.format(time);
+        }
+
+        private String formatHighLows(double high, double low) {
+
+            long roundedHigh = Math.round(high);
+            long roundedLow = Math.round(low);
+
+            String highLowStr = roundedHigh + "/" + roundedLow;
+            return highLowStr;
+        }
+
+        private String[] getWeatherDataFromJson(String forecastJsonStr, int numDays)
+                throws JSONException {
+
+            final String OWM_LIST = "list";
+            final String OWM_WEATHER = "weather";
+            final String OWM_TEMPERATURE = "temp";
+            final String OWM_MAX = "max";
+            final String OWM_MIN = "min";
+            final String OWM_DESCRIPTION = "main";
+
+            JSONObject forecastJson = new JSONObject(forecastJsonStr);
+            JSONArray weatherArray = forecastJson.getJSONArray(OWM_LIST);
+
+            Time dayTime = new Time();
+            dayTime.setToNow();
+
+            int julianStartDay = Time.getJulianDay(System.currentTimeMillis(), dayTime.gmtoff);
+
+            dayTime = new Time();
+
+            String[] resultStrs = new String[numDays];
+            for(int i = 0; i < weatherArray.length(); i++) {
+                String day;
+                String description;
+                String highAndLow;
+
+                JSONObject dayForecast = weatherArray.getJSONObject(i);
+
+                long dateTime;
+                dateTime = dayTime.setJulianDay(julianStartDay+i);
+                day = getReadableDateString(dateTime);
+
+                JSONObject weatherObject = dayForecast.getJSONArray(OWM_WEATHER).getJSONObject(0);
+                description = weatherObject.getString(OWM_DESCRIPTION);
+
+                JSONObject temperatureObject = dayForecast.getJSONObject(OWM_TEMPERATURE);
+                double high = temperatureObject.getDouble(OWM_MAX);
+                double low = temperatureObject.getDouble(OWM_MIN);
+
+                highAndLow = formatHighLows(high, low);
+                resultStrs[i] = day + " - " + description + " - " + highAndLow;
+            }
+
+
+            for (String s : resultStrs) {
+                Log.v(LOG_TAG, "Forecast entry: " + s);
+            }
+
+            return resultStrs;
+
+        }
 
         @Override
-        protected String doInBackground(String... params) {
+        protected String[] doInBackground(String... params) {
 
 
             HttpURLConnection urlConnection = null;
@@ -66,8 +171,19 @@ public class ForecastFragment extends Fragment {
 
             String forecastJsonStr = null;
 
+            final String FORECAST_BASE_URL = "http://api.openweathermap.org/data/2.5/forecast/daily?";
+            final String MODE = "json";
+            final String METRIC = "metric";
+            final int NUM_DAYS = 7;
             try {
-                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7");
+
+                Uri builderURI = Uri.parse(FORECAST_BASE_URL).buildUpon()
+                        .appendQueryParameter("q", params[0])
+                        .appendQueryParameter("mode",MODE)
+                        .appendQueryParameter("units",  METRIC)
+                        .appendQueryParameter("cnt",  String.valueOf(NUM_DAYS)).build();
+
+                URL url = new URL(builderURI.toString());
 
                 urlConnection = (HttpURLConnection) url.openConnection();
                 urlConnection.setRequestMethod("GET");
@@ -105,7 +221,24 @@ public class ForecastFragment extends Fragment {
                 }
             }
 
-            return forecastJsonStr;
+            try {
+                return getWeatherDataFromJson(forecastJsonStr,NUM_DAYS);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return  null;
+        }
+
+        @Override
+        protected void onPostExecute(String[] strings) {
+            if(strings != null){
+
+                adapter.clear();
+
+                for ( String result: strings)
+                    adapter.add(result);
+            }
         }
     }
 }
